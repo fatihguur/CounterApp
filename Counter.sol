8d6da393ac798a65f77c584f10356683945ef4d7
Fatih Gur <fatih.gur26@hotmail.com>
2023-01-03 14:10:30 +0300

    Comments

--- Counter.sol
+++ Counter.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.8.7;
 contract Counter {
     
     uint public count;
-
+    
+    // Adds 1 to count
     function Increase () external {
         count++;
     }
 
+    // Subs Subtracts 1 from count
     function Decrease () external {
         count--;
     }
